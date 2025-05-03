a072a6a9c4a3f3ca06ed04b6ec47c6d0a8100d1f
Gabriel-ISS <gabrielsykora693@gmail.com>
2025-05-03 16:55:59 -0300

    Corrección en botón de cerrar juego
    Ahora en web ya no aparece

--- Scenes/UI/start_screen.gd
+++ Scenes/UI/start_screen.gd
@@ -1,4 +1,13 @@
 extends CanvasLayer
 
+@export var exit_btn: Button
+
+
+func _ready() -> void:
+	if OS.get_name() == 'Web':
+		exit_btn.visible = false
+		exit_btn.process_mode = Node.PROCESS_MODE_DISABLED
+
+
 func _on_exit_btn_pressed() -> void:
 	OS.kill(OS.get_process_id())
